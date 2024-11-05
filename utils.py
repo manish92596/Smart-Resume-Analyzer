b93ad0eaae1824731ba0724a34dce5268dd1a60b
manish92596 <manishkumar92596@gmail.com>
2024-11-06 00:07:20 +0530

    new changes

--- utils.py
+++ utils.py
@@ -1,13 +1,33 @@
-
-
 import streamlit as st
 import spacy
 from sentence_transformers import SentenceTransformer
+import subprocess
 
 @st.cache_resource
 def load_nlp_model():
-    return spacy.load("en_core_web_sm")
+    # Check if the model is available, and download if not
+    try:
+        return spacy.load("en_core_web_sm")
+    except OSError:
+        # Model is missing, so download it
+        subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+        return spacy.load("en_core_web_sm")
 
 @st.cache_resource
 def load_transformer_model():
     return SentenceTransformer('all-MiniLM-L6-v2')
+
+
+
+
+# import streamlit as st
+# import spacy
+# from sentence_transformers import SentenceTransformer
+
+# @st.cache_resource
+# def load_nlp_model():
+#     return spacy.load("en_core_web_sm")
+
+# @st.cache_resource
+# def load_transformer_model():
+#     return SentenceTransformer('all-MiniLM-L6-v2')
